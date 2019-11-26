32019dc22b24e072a4ec027f88b3d429f9dddeca
saranshgrover <ycubiksrube@gmail.com>
2019-11-26 17:41:52 -0500

    Add Navbar

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,42 +1,46 @@
-import React from 'react'
-import { Redirect } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
+
+import { FirebaseContext } from '../../utils/firebase'
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1
   },
   title: {
-    flexGrow: 1
+    textDecoration: 'none',
+    color: 'white',
+    flexGrow: 1,
+    marginLeft: theme.spacing(1)
   }
 }))
 
-export default function Navbar({ history, signedIn, user, handleLogout }) {
+export default function Navbar({ history, signedIn, handleLogout }) {
   const classes = useStyles()
+  const firebase = useContext(FirebaseContext)
   const handleClick = () => {
     !signedIn ? history.push('/login') : handleLogout()
   }
   return (
-    <div className={classes.root}>
-      <AppBar position='fixed'>
-        <Toolbar>
-          <Typography variant='h6' className={classes.title}>
-            Wireframer
-          </Typography>
-          <Button onClick={handleClick} color='inherit'>
-            {signedIn ? 'Logout' : 'Login'}
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar position='sticky' color='primary' className={classes.appBar}>
+      <Toolbar spacing={2} className={classes.title}>
+        <Typography
+          variant='h6'
+          className={classes.title}
+          component={Link}
+          to='/'
+        >
+          Wireframer
+        </Typography>
+        <Button onClick={handleClick} color='inherit'>
+          {signedIn ? 'Logout' : 'Login'}
+        </Button>
+      </Toolbar>
+    </AppBar>
   )
 }
