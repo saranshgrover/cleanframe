79d83b786bc129d8f1be5acfcdc0bd451034dea4
saranshgrover <ycubiksrube@gmail.com>
2019-11-28 20:35:16 -0500

    Add avatar to navbar

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
@@ -7,6 +7,7 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 
 import { FirebaseContext } from '../../utils/firebase'
+import { Avatar } from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
   appBar: {
@@ -17,12 +18,26 @@ const useStyles = makeStyles(theme => ({
     color: 'white',
     flexGrow: 1,
     marginLeft: theme.spacing(1)
+  },
+  avatar: {
+    backgroundColor: theme.palette.secondary.main
   }
 }))
 
 export default function Navbar({ history, signedIn, handleLogout }) {
   const classes = useStyles()
   const firebase = useContext(FirebaseContext)
+  const [userInfo, setUserInfo] = useState(null)
+  useEffect(() => {
+    const user = firebase.auth().currentUser
+    signedIn &&
+      firebase
+        .firestore()
+        .collection('users')
+        .doc(user.uid)
+        .get()
+        .then(resp => setUserInfo(resp.data()))
+  }, [signedIn, firebase])
   const handleClick = () => {
     !signedIn ? history.push('/login') : handleLogout()
   }
@@ -37,9 +52,19 @@ export default function Navbar({ history, signedIn, handleLogout }) {
         >
           Wireframer
         </Typography>
-        <Button onClick={handleClick} color='inherit'>
-          {signedIn ? 'Logout' : 'Login'}
-        </Button>
+        {signedIn && userInfo && (
+          <Button onClick={handleClick}>
+            <Avatar className={classes.avatar} variant='square'>
+              {userInfo.firstName[0]}
+            </Avatar>
+          </Button>
+        )}
+        {!signedIn && (
+          <Button onClick={handleClick} color='inherit'>
+            {' '}
+            Login
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
   )
